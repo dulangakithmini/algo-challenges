55a55fbe97150028726377536c60c75a1d46220c
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-05-10 21:02:24 +0530

    Check for Anagram strings

--- src/Anagram.java
+++ src/Anagram.java
@@ -9,10 +9,6 @@ public class Anagram {
             char[] array1 = text1.toCharArray();
             char[] array2 = text2.toCharArray();
 
-            for (int i = 0; i < text1.length(); i++) {
-                array1[i] = text1.charAt(i);
-                array2[i] = text2.charAt(i);
-            }
             Arrays.sort(array1);
             Arrays.sort(array2);
 
