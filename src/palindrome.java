3f050db9e75ba176cc88431ac13104dd3237a3d0
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-05-10 16:06:00 +0530

    Check whether a string is a palindrome

---
+++ src/palindrome.java
@@ -0,0 +1,17 @@
+public class palindrome {
+    public static void main(String[] args) {
+        String text = "aebewa";
+        boolean flag = true;
+
+        for (int i=0; i<text.length()/2; i++) {
+            if (text.charAt(i) != text.charAt(text.length()-1-i)) {
+                System.out.println("Not a Palindrome");
+                flag = false;
+                break;
+            }
+        }
+        if (flag) {
+            System.out.println("Palindrome");
+        }
+    }
+}
